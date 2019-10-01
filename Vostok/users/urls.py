4ee3edc687dd8115fb7448a20e532cf6bf63e8e8
Andres Quiroz <andresq1595@gmail.com>
2019-10-01 11:17:54 -0500

    Redirect log in

--- Vostok/users/urls.py
+++ Vostok/users/urls.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.urls import path
 from django.urls import include
 from users.views import index
+from users.views import login
 
 
 urlpatterns = [
+    path('',login),
     path('auth/google/', include('social_django.urls', namespace='social')),
     path('index/',index),
     #path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL},
