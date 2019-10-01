d43433305823bc67fa57f4ee1cb1efdd48003913
Andres Quiroz <andresq1595@gmail.com>
2019-10-01 12:14:43 -0500

    Postgres DB

--- Vostok/Vostok/settings.py
+++ Vostok/Vostok/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Vostok.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Vostok',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
