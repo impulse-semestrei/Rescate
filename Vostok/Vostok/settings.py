fbea87574488193c3976103c7a173cb012c07385
EduardoCuestaC <A00570171@itesm.mx>
2019-10-02 12:07:59 -0500

    add setup_instructions

--- Vostok/Vostok/settings.py
+++ Vostok/Vostok/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Vostok',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
