d618459076446427a36fc4af966807afcd9e0971
ReneOV <a01702860@itesm.mx>
2019-10-08 12:21:25 -0500

    Crear app dashboard, y base de frontend

---
+++ Vostok/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ReneOv\Desktop\Move to HDD\ITESM\Semestre 6\Git Repo Web\Vostok\Vostok\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
