48a513ad03510d5ec8249584a4352e128b86fe7d
ReneOV <a01702860@itesm.mx>
2019-10-15 01:28:11 -0500

    Finish front-end for US 39

--- Vostok/Vostok/settings.py
+++ Vostok/Vostok/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Vostok',
-        'USER': 'arturo',
-        'PASSWORD': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1234567890',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
