9913d39dae774b7d106436ca59a95ff208454fd6
Andres Quiroz <andresq1595@gmail.com>
2019-10-21 20:25:08 -0500

    Fixes

--- Vostok/revision/views.py
+++ Vostok/revision/views.py
@@ -9,10 +9,10 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def ver_revisiones(request, id):
+
     ambulancia = Ambulancia.objects.get(id=id)
     inventario = ambulancia.inventario
     revisiones = Revision.objects.filter(inventario=inventario.id)
-
     context={'revisiones': revisiones}
     return render(request, '../templates/revision/ver_revisiones.html', context)
 ##### CONTROLLER US09 #####
