f5d210d9595180bfd57b9576d3df55f0d4befc2e
Paulo <a01207593@itesm.mx>
2019-10-25 12:54:22 -0500

    Arreglar editar material

--- Vostok/material/urls.py
+++ Vostok/material/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('crear/', views.crear_material, name='crear'),
     path('ver/', views.ver_material, name='ver_material'),
     path('delete/<int:id>/', views.delete_material, name="delete_material"),
-    path('editar/<int:id>/', views.editar_material, name="edit_material"),
+    path('editar/<int:id>/', views.editar_material, name="edit"),
+    path('editar_material/<int:id>/', views.editar_material_view, name="editar_material_view"),
 ]
