3333840d65bb23c50bf233cd2171a7e4b8ecf5cd
leco96 <a01206220@itesm.mx>
2019-11-26 12:05:41 -0600

    correcciones_id

--- Vostok/Vostok/settings.py
+++ Vostok/Vostok/settings.py
@@ -114,7 +114,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'Vostok',
             'USER': 'postgres',
-            'PASSWORD': 'PauloSolis1',
+            'PASSWORD': 'postgres',
             'HOST': '127.0.0.1',
             'PORT': '5432',
         }
