22dfd697287915570b78902b43b3cc5978f29e9a
leco96 <a01206220@itesm.mx>
2019-12-04 10:17:14 -0600

    id

--- Vostok/material/models.py
+++ Vostok/material/models.py
@@ -7,7 +7,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Material(models.Model):
-    codigo = models.CharField(max_length=100, unique=True, null=True)
     nombre = models.CharField(max_length=100, unique=True, null=False)
     descripcion = models.TextField(null=False)
     cantidad = models.IntegerField(null=False, validators=[MaxValueValidator(10000), MinValueValidator(0)])
